6f3e15c226e6ddd769acf2cdf73102d730c5c1b5
zeycoder <zynparsln.5561@gmail.com>
2022-06-27 11:58:28 +0300

    Update App.js

--- proje2/App.js
+++ proje2/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert} from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -20,12 +20,20 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ];
-    })
+    if(text.length > 2){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      })
+    }else{
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
+
+    
   }
 
   return (
