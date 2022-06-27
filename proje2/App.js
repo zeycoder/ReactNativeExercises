2312c564facdaea4549dbc67a68f63f2577811f0
zeycoder <zynparsln.5561@gmail.com>
2022-06-27 11:40:36 +0300

    added 'add todo'

--- proje2/App.js
+++ proje2/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem';
-
+import AddTodo from './components/addTodo';
 
 export default function App() {
 
@@ -19,11 +19,20 @@ export default function App() {
     });
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ];
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header/>
       <View style={styles.content}>
-        {/*to form */}
+        <AddTodo submitHandler={submitHandler}/>
         <View style={styles.list}>
           <FlatList
             data={todos}
