b8f2c626b3edc3ea613fe6d00902a2347d1663b5
zeycoder <zynparsln.5561@gmail.com>
2022-06-27 10:37:20 +0300

    todos updated

--- proje2/App.js
+++ proje2/App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header'
+import TodoItem from './components/todoItem';
+
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key:'1'},
+    { text: 'create an app', key: '2'},
+    { text: 'play on the switch', key: '3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View style={styles.content}>
+        {/*to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +41,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  content: {
+    padding: 40,
+  },
+  list:{
+    marginTop: 20,
+  }
 });
