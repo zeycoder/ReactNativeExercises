6a38de0f1c382f3d698db364d5d60c13474d3c4d
zeycoder <zynparsln.5561@gmail.com>
2022-06-27 09:19:41 +0300

    add list

--- proje1/App.js
+++ proje1/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [people, setPeople] = useState([
+    { name: 'zey', id:'1'},
+    { name: 'erva', id:'2'},
+    { name: 'alp', id:'3'},
+    { name: 'muzo',id:'4'},
+    { name: 'süho', id:'5'},
+    { name: 'haso', id:'6'},
+    { name: 'fero', id:'7'},
+    { name: 'sudo', id:'8'}
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item)=> item.id }
+        data={people}
+        renderItem={({item}) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
     </View>
   );
 }
@@ -14,7 +33,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:40,
+    paddingHorizontal: 20
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  item:{
+    marginTop: 24,
+    padding:30,
+    backgroundColor:'purple',
+    fontSize:24,
+    marginHorizontal:10,
+    marginTop:20,
+
   },
 });
