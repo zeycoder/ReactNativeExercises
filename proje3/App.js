541e17ca8857149d4e7f70f2d6e918c62ee56d10
zeycoder <zynparsln.5561@gmail.com>
2022-06-28 16:23:54 +0300

    Project changed

--- proje3/App.js
+++ proje3/App.js
@@ -1,12 +1,71 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { StyleSheet, View, Text } from "react-native";
-import Home from './screens/home';
-import About from './screens/about';
-import Navigator from './routes/homeStack'; 
+import * as React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <Navigator />    
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      />
+    </View>
   );
 }
+
+function DetailsScreen({ route, navigation }) {
+
+  const { itemId, otherParam } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export const styles = StyleSheet.create({
+  container:{
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+});
+
+export default App;
