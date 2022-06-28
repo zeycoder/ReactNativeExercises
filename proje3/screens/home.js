20333a3a573a62725db85568c57c2324ec20b76f
zeycoder <zynparsln.5561@gmail.com>
2022-06-28 09:40:26 +0300

    Packages added

---
+++ proje3/screens/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Home(){
+    return(
+        <View style={styles.container}>
+            <Text>Home Screen!</Text>
+             
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding:24
+    },
+});
