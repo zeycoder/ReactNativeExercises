459e91f925eef427e8f9758f53410726930e21bd
zeycoder <zynparsln.5561@gmail.com>
2022-06-28 11:14:13 +0300

    packages have been updated

--- proje3/App.js
+++ proje3/App.js
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, View, Text } from "react-native";
 import Home from './screens/home';
 import About from './screens/about';
+import Navigator from './routes/homeStack'; 
 
 export default function App() {
   return (
-    <Home />    
+    <Navigator />    
   );
 }
