f5d753823eff5b3f7bf99e934f8c6e415d963fc3
zeycoder <zynparsln.5561@gmail.com>
2022-06-29 17:17:58 +0300

    Update App.js

--- proje4/App.js
+++ proje4/App.js
@@ -12,10 +12,11 @@ function HomeScreen( {navigation} ) {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button title='Home page' onPress={() => navigation.navigate('Home')} />
     </View>
   );
 }
