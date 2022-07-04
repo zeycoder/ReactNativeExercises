82d3b747340e7ed8dbb5a35191d030516fc9434f
zeycoder <zynparsln.5561@gmail.com>
2022-07-04 15:03:50 +0300

    Update App.js

--- proje5/App.js
+++ proje5/App.js
@@ -4,6 +4,7 @@ import { SafeAreaView, Text, Button } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Home = ({navigation}) =>{
   return (
@@ -42,7 +43,41 @@ const App = () => {
         <MainStack.Screen name="Home" component={Home} />
         <MainStack.Screen name="Settings" component={Settings} />
       </MainStack.Navigator> */}
-      <Tabs.Navigator>
+      <Tabs.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            if (route.name === 'Message') {
+              iconName = focused
+                ? 'chatbubbles'
+                : 'chatbubbles-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+            
+            if (route.name === 'Profile') {
+              iconName = focused
+                ? 'md-person'
+                : 'md-person-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tabs.Screen name="Profile" component={Profile} />
         <Tabs.Screen name="Home" component={Home} />
         <Tabs.Screen name="Message" component={Message} />
