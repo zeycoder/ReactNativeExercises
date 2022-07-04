bd860c34f581959becabe63a1172246219c406f6
zeycoder <zynparsln.5561@gmail.com>
2022-07-04 13:47:12 +0300

    Update App.js

--- proje5/App.js
+++ proje5/App.js
@@ -1,22 +1,33 @@
 import React from "react";
 import Chats from "./screens/Chats";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, Button } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-const Home = () =>{
+const Home = ({navigation}) =>{
   return (
     <SafeAreaView>
       <Text>hi, home</Text>
+      <Button 
+        title="go to settings screen"
+        onPress={()=>{
+          navigation.push('Settings')
+        }}
+      />
     </SafeAreaView>
     )
 }
 
-const Settings = () => {
+const Settings = ({navigation}) => {
   return(
     <SafeAreaView>
       <Text>hi, settings</Text>
+      <Button 
+        title="go to home screen"
+        onPress={() => {
+          navigation.goBack('Home')
+        }} />
     </SafeAreaView>
   )
 }
