d52e4aa5f17b9e03bf8968ca890511e939206f9a
zeycoder <zynparsln.5561@gmail.com>
2022-07-04 11:29:04 +0300

    Update App.js

--- proje5/App.js
+++ proje5/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import {NavigationContainer} from '@react-natigation/nat,ve'
 import Chats from "./screens/Chats";
+import { SafeAreaView } from "react-native";
+import {createStackNavigator} from 'react-navigation/stack'
+
+const Home = () =>{
+  return (
+    <SafeAreaView>
+      <Text>hi, home</Text>
+    </SafeAreaView>
+    )
+}
+
+const Settings = () => {
+  return(
+    <SafeAreaView>
+      <Text>hi, settings</Text>
+    </SafeAreaView>
+  )
+}
 
 const App = () => {
   return(
-    <Chats />
+    <NavigationContainer>
+
+    </NavigationContainer>
   )
 }
 
