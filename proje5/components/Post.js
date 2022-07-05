a9102fd2395012523c4a819aecbe05d056e005db
zeycoder <zynparsln.5561@gmail.com>
2022-07-05 11:15:18 +0300

    Made dinamic

---
+++ proje5/components/Post.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, StyleSheet, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { colors } from "../config/constants";
+
+const Post = ({name, onPress}) => {
+    return(
+        <SafeAreaView>
+            <TouchableOpacity style={styles.row} onPress={onPress} >
+                <View style={styles.avatar} >
+                    <Text style={styles.avatarLaber}>{name.split(' ').reduce((prev,current) => `${prev[0]}${current[0]}`)}</Text>
+                </View>
+                <Text style={styles.avatarLaber}> {name}</Text>
+            </TouchableOpacity>
+            <View style={styles.post} />
+            <View style={styles.space} />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    post:{
+        width:'100%',
+        height:400,
+        backgroundColor:colors.primary,
+        
+    },
+    avatar:{
+        width:38,
+        height:38,
+        backgroundColor: colors.primary,
+        borderRadius:28,
+        alignItems:'center',
+        justifyContent:'center',
+        marginRight:12
+    },
+    avatarLaber:{
+        fontSize:16,
+    },
+    row:{
+        flexDirection:'row',
+        alignItems:'center',
+        paddingHorizontal:10,
+        paddingVertical:10
+    },
+    space:{
+        width:'100%',
+        height:40,
+    },
+})
+
+export default Post;
--- proje5/screens/Home.js
+++ proje5/screens/Home.js
@@ -1,74 +1,27 @@
 import React from "react";
 import { SafeAreaView, Text, StyleSheet, View, ScrollView, TouchableOpacity } from "react-native";
 import { colors } from "../config/constants";
+import Post from "../components/Post";
 
 const Home = () => {
     return(
         <SafeAreaView>
             <ScrollView>
-                <TouchableOpacity>
-                    <View style={styles.row}>
-                        <View style={styles.avatar} />
-                        <Text style={styles.avatarLaber}>Zeynep Arslan</Text>
-                    </View>
-                </TouchableOpacity>
-                <View style={styles.post} />
-
-                <View style={styles.space} />
-
-                <TouchableOpacity>
-                    <View style={styles.row}>
-                        <View style={styles.avatar} />
-                        <Text style={styles.avatarLaber}>Zeynep Arslan</Text>
-                    </View>
-                </TouchableOpacity>
-                <View style={styles.post} />
-
-                <View style={styles.space} />
-
-                <TouchableOpacity>
-                    <View style={styles.row}>
-                        <View style={styles.avatar} />
-                        <Text style={styles.avatarLaber}>Zeynep Arslan</Text>
-                    </View>
-                </TouchableOpacity>
-                <View style={styles.post} />
+                <Post 
+                    name='Zeynep Arslan'
+                    onPress={() => {
+                        alert('Hi, Zeynep Arslan Touched')
+                    }}
+                />
+                <Post 
+                    name='Erva Arslan'
+                    onPress={() => {
+                        alert('Hi, Erva Arslan Touched')
+                    }}
+                />   
             </ScrollView>
         </SafeAreaView>
     )
 }
-
-
-const styles = StyleSheet.create({
-    post:{
-        width:'100%',
-        height:400,
-        backgroundColor:colors.primary,
-        
-    },
-    avatar:{
-        width:38,
-        height:38,
-        backgroundColor: colors.primary,
-        borderRadius:28,
-        alignItems:'center',
-        justifyContent:'center',
-        marginRight:12
-    },
-    avatarLaber:{
-        fontSize:16,
-    },
-    row:{
-        flexDirection:'row',
-        alignItems:'center',
-        paddingHorizontal:10,
-        paddingVertical:10
-    },
-    space:{
-        width:'100%',
-        height:40,
-    },
-})
-
 
 export default Home;
