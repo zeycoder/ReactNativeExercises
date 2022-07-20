384f54d06988a1f5b5b7fec874044ee0ab000739
zeycoder <zynparsln.5561@gmail.com>
2022-07-20 11:55:25 +0300

    Update App.js

--- proje7/App.js
+++ proje7/App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet,View, Text, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  
+  const [todos, setTodos] = useState([])
+  const [submit, setSubmit] = useState(false)
+
+  useEffect(() => {
+    async function getTodos() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+      const data = await response.json()
+      console.log(data)
+      setTodos(data)
+    }
+    if(submit){
+      getTodos()
+    }
+  },[submit])
+  function fetchTodos(){
+    setSubmit(true)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.safearea} >
+          <Button title='Tıkla' onPress={fetchTodos}></Button>
+        </View>
+        {todos.map((todo) => {
+          return (
+            <Text key={todo.id}> {todo.id} -- {todo.title} .. {todo.completed ? "true" ||"pwldkodl" : "false" }</Text>
+          )
+        })}
+        
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -14,7 +44,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 15,
+  },
+  safearea:{
+    marginTop:20,
+    padding:50,
   },
 });
