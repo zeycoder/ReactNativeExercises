66dd68b62dae1c0535efc1275441c61b12a05dfb
zeycoder <zynparsln.5561@gmail.com>
2022-07-20 18:03:04 +0300

    Update App.js

--- proje7/App.js
+++ proje7/App.js
@@ -1,51 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet,View, Text, Button, ScrollView } from 'react-native';
 
+import { useState } from 'react';
+import { StyleSheet,View, Text, Button, TextInput } from 'react-native';
+
+//Girilen inputları apiye yollama örneğidir.
 export default function App() {
   
   const [todos, setTodos] = useState([])
-  const [submit, setSubmit] = useState(false)
 
-  useEffect(() => {
-    async function getTodos() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-      const data = await response.json()
-      console.log(data)
-      setTodos(data)
-    }
-    if(submit){
-      getTodos()
-    }
-  },[submit])
-  function fetchTodos(){
-    setSubmit(true)
-  }
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.safearea} >
-          <Button title='Tıkla' onPress={fetchTodos}></Button>
-        </View>
-        {todos.map((todo) => {
-          return (
-            <View style={styles.g} >
-              <Text 
-                key={todo.id} 
-              >{todo.id} </Text>
-              <Text 
-                key={todo.id} 
-                style={ todo.completed ?  [styles.true] : [styles.false] } 
-              > -- {todo.title} .. {todo.completed ? "true" : "false" }
-              </Text>
-            </View>
-          )
-        })}
-        
-        <StatusBar style="auto" />
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder='bilgi giriniz.'
+      />
+      <Button style={styles.button} title='Tıkla ve Gonder' ></Button>
+    </View>
   );
 }
 
@@ -53,19 +23,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    margin: 15,
+    marginTop: 50,
+    paddingHorizontal: 15
   },
-  safearea:{
-    marginTop:20,
-    padding:50,
+  input:{
+    alignItems:'center',
+    borderWidth: 2,
+    padding: 10
   },
-  true:{
-    color: 'green',
+  button:{
+    paddingVertical:20,
   },
-  false:{
-    color:'red',
-  },
-  g:{
-    flexDirection:'row'
-  },
+  
 });
