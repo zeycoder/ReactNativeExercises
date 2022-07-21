9baf3a98a4316d557db6be01bb0ffac585cff2c2
zeycoder <zynparsln.5561@gmail.com>
2022-07-21 10:43:57 +0300

    Update App.js

--- proje7/App.js
+++ proje7/App.js
@@ -1,21 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, ScrollView, TextInput } from 'react-native';
 
-import { useState } from 'react';
-import { StyleSheet,View, Text, Button, TextInput } from 'react-native';
-
-//Girilen inputları apiye yollama örneğidir.
+// Apiye veri gönderme, useState ve useEffect kullanma örneğidir.
 export default function App() {
   
   const [todos, setTodos] = useState([])
+  const [submit, setSubmit] = useState(false)
+  const [text, setText] = useState('')
 
+  useEffect(() => {
+    async function getTodos() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          text
+        })
+      })
+      const data = await response.json()
+      setSubmit(false)
+      setText('')
+      console.log(data)
+    }
+    if(submit){
+      getTodos()
+    }
+  },[submit])
+  function fetchTodos(){
+    setSubmit(true)
+  }
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder='bilgi giriniz.'
-      />
-      <Button style={styles.button} title='Tıkla ve Gonder' ></Button>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.safearea} >
+          <Text style={{fontSize:25, margin:8}} >Apiden çekilen bilgiler</Text>
+          <TextInput onChangeText={(e) => setText(e)} style={styles.input} defaultValue={text} placeholder='İstediğinizi giriniz.'/>
+          <Button title='Tıkla ve Listele' onPress={fetchTodos}></Button>
+        </View>
+        {todos.map((todo) => {
+          return (
+            <View style={styles.g} >
+              <Text 
+                key={todo.id} 
+              >{todo.id} </Text>
+              <Text 
+                key={todo.id} 
+                style={ todo.completed ?  [styles.true] : [styles.false] } 
+              > -- {todo.title} .. {todo.completed ? "true" : "false" }
+              </Text>
+            </View>
+          )
+        })}
+        
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -23,16 +66,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 50,
-    paddingHorizontal: 15
+    margin: 15,
+  },
+  safearea:{
+    marginTop:20,
+    padding:50,
+  },
+  true:{
+    color: 'green',
+  },
+  false:{
+    color:'red',
+  },
+  g:{
+    flexDirection:'row'
   },
   input:{
     alignItems:'center',
     borderWidth: 2,
-    padding: 10
+    padding: 10,
+    marginBottom:15,
   },
-  button:{
-    paddingVertical:20,
-  },
-  
 });
