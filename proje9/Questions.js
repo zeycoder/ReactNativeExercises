090870c88cc338ff461dd21fdda84035092396d8
zeycoder <zynparsln.5561@gmail.com>
2022-07-22 10:22:51 +0300

    Create Questions.js

---
+++ proje9/Questions.js
@@ -0,0 +1,30 @@
+import { Text } from "react-native";
+
+const question = [
+    {
+        id:1,
+        soru:"Ben kaç yaşındayım ? ", 
+        cevap:"21",
+        a:"21",
+        b:"22",
+        c:"23",
+        d:"24"
+    },{
+        id:2,
+        soru:"Günde kaç öğün yemek yerim ? ",
+        cevap:"4",
+        a:"1",
+        b:"2",
+        c:"3",
+        d:"4"
+    },{
+        id:3,
+        soru:"Stajım kaç iş günüdür ? ",
+        cevap:"40",
+        a:"15",
+        b:"20",
+        c:"40",
+        d:"45"
+    }
+]
+export default Questions;
