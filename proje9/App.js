58bf24d7c6b6feffac38deebb69ca6ed2631636f
zeycoder <zynparsln.5561@gmail.com>
2022-07-22 10:23:27 +0300

    Update App.js

--- proje9/App.js
+++ proje9/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Css from './css';
+import { styles } from './css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text style={Css.style.a} >wpedoşwdlep</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
