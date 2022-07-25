c70acee5f31d5dead772245170c7dbf15f7dd168
zeycoder <zynparsln.5561@gmail.com>
2022-07-25 15:44:50 +0300

    Update App.js

--- proje9/App.js
+++ proje9/App.js
@@ -1,28 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { styles } from './Css';
 import { question } from './Questions';
 
 export default function App() {
+
+  const[answer,setAnswer] = useState()
+
   return (
-    <View style={styles.average} >
-      {question.map(liste => <Text style={styles.questions} > {liste.soru} </Text>)} 
-      
-      <TouchableOpacity style={styles.button} >
-        {question.map(liste => <Text style={styles.text}> A)  {liste.a} </Text> )}
-      </TouchableOpacity >
+    <View style={styles.average} >      
+        {question.map(liste => {
+          return <View>
+            <Text style={styles.questions} > {liste.soru} </Text>
+            <TouchableOpacity style={styles.button}><Text style={styles.text}> A)  {liste.a} </Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.text}> B)  {liste.b} </Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.text}> C)  {liste.c} </Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button}><Text style={styles.text}> D)  {liste.d} </Text></TouchableOpacity>
+          </View>
+        } )}
 
-      <TouchableOpacity style={styles.button} >
-        {question.map(liste => <Text style={styles.text}> B)  {liste.b} </Text> )}
-      </TouchableOpacity >
-
-      <TouchableOpacity style={styles.button} >
-        {question.map(liste => <Text style={styles.text}> C)  {liste.c} </Text> )}
-      </TouchableOpacity >
-
-      <TouchableOpacity style={styles.button} >
-        {question.map(liste => <Text style={styles.text}> D)  {liste.d} </Text> )}
-      </TouchableOpacity >
+  
 
       <StatusBar style="auto" />
     </View>
