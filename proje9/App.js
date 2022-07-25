5e681b63b60515d424cc8914d696bdb1ebdc05f2
zeycoder <zynparsln.5561@gmail.com>
2022-07-25 14:43:36 +0300

    Update App.js

--- proje9/App.js
+++ proje9/App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { styles } from './Css';
 import { question } from './Questions';
 
 export default function App() {
   return (
     <View style={styles.average} >
-      {question.map(liste => <Text> SORU: {liste.soru} </Text>)} 
+      {question.map(liste => <Text style={styles.questions} > {liste.soru} </Text>)} 
+      
+      <TouchableOpacity style={styles.button} >
+        {question.map(liste => <Text style={styles.text}> A)  {liste.a} </Text> )}
+      </TouchableOpacity >
+
+      <TouchableOpacity style={styles.button} >
+        {question.map(liste => <Text style={styles.text}> B)  {liste.b} </Text> )}
+      </TouchableOpacity >
+
+      <TouchableOpacity style={styles.button} >
+        {question.map(liste => <Text style={styles.text}> C)  {liste.c} </Text> )}
+      </TouchableOpacity >
+
+      <TouchableOpacity style={styles.button} >
+        {question.map(liste => <Text style={styles.text}> D)  {liste.d} </Text> )}
+      </TouchableOpacity >
 
       <StatusBar style="auto" />
     </View>
