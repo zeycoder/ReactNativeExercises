2711e14fb304d7889a400a9273c3176cfc063729
zeycoder <zynparsln.5561@gmail.com>
2022-07-25 14:44:00 +0300

    Update Css.js

--- proje9/Css.js
+++ proje9/Css.js
@@ -2,9 +2,28 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     average:{
-        justifyContent:'center',
+        flex:1,
         alignItems:'center',
-        textAlign:'center',
+        justifyContent:'center',
+        borderWidth:5,
+        backgroundColor:'#FFC8FF',
     },
-    
+    questions:{
+        fontSize:25,
+        padding:25,
+    },
+    button:{
+        height:50,
+        width:300,
+        justifyContent:'center',
+        backgroundColor:'#FFD1FF',
+        borderColor:'purple',
+        borderWidth:2,
+        borderRadius:15,
+        paddingHorizontal:10,
+        marginVertical:8,
+    },
+    text:{
+        fontSize:20
+    }
 })
