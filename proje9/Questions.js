ee90fcfeb96f8961edd85e1b64feb7153d8c064e
zeycoder <zynparsln.5561@gmail.com>
2022-07-25 14:44:13 +0300

    Update Questions.js

--- proje9/Questions.js
+++ proje9/Questions.js
@@ -1,13 +1,11 @@
-import { Text } from "react-native";
-
 export const question = [
     {
         id:1,
-        soru:"Ben kaç yaşındayım ? ", 
-        cevap:"21",
-        a:"21",
-        b:"22",
-        c:"23",
-        d:"24"
+        soru:"Nerede yaşıyoruz ? ", 
+        cevap:"Dünya",
+        a:"Jüpiter",
+        b:"Satürn",
+        c:"Dünya",
+        d:"Mars"
     }
 ]
