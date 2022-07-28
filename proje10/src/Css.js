7d5e986a3083e6db0833eb48933fb6fe50ada061
zeycoder <zynparsln.5561@gmail.com>
2022-07-28 15:22:51 +0300

    Update Css.js

--- proje10/src/Css.js
+++ proje10/src/Css.js
@@ -4,8 +4,6 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        alignItems:'center',
-        justifyContent:'center',
         marginTop:50,
     },
     textInput:{
